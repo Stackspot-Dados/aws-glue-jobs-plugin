52d8ddc24d807f4734435d5c21d1ec2760c29834
Elias Nepomuceno <eliasnepo12@gmail.com>
2022-08-09 10:36:31 -0300

    Added default_arguments to jobs.tf

--- templates/jobs.tf
+++ templates/jobs.tf
@@ -5,6 +5,13 @@ resource "aws_glue_job" "glue_job_{{inputs.job_name}}" {
     timeout         = {{inputs.timeout}}
     glue_version    = "{{inputs.glue_version}}"
     max_retries     = {{inputs.max_retries}}
+    {% if inputs.job_type == 'pythonshell' %}
+    max_capacity = {{inputs.max_capacity}}
+    {% endif %}
+    {% if inputs.job_type == 'glueetl' %}
+    worker_type         = "{{inputs.worker_type}}"
+    number_of_workers   = {{inputs.number_of_workers}}
+    {% endif %}
 
     execution_property {
         max_concurrent_runs = {{inputs.concurrent_runs}}
@@ -15,12 +22,16 @@ resource "aws_glue_job" "glue_job_{{inputs.job_name}}" {
         script_location     = "{{inputs.script_location}}"
     }
 
-    {% if inputs.job_type == 'pythonshell' %}
-    max_capacity = {{inputs.max_capacity}}
-    {% endif %}
-
-    {% if inputs.job_type == 'glueetl' %}
-    worker_type         = "{{inputs.worker_type}}"
-    number_of_workers   = {{inputs.number_of_workers}}
-    {% endif %}
+    default_arguments {
+        {% if inputs.job_type == 'glueetl' and inputs.glue_version == '3.0' %}
+        "--enable-auto-scaling"    = true
+        {% endif %}
+        {% if inputs.extra_py_files == true %}
+        "--extra-py-files"         = "{{inputs.extra_py_files_path}}"
+        {% endif %}
+        {% if inputs.extra_jars == true %}
+        "--extra-jars"             = "{{inputs.extra_jars_path}}"
+        {% endif %}
+        # Inserir parâmetros do jobs aqui
+    }
 }
